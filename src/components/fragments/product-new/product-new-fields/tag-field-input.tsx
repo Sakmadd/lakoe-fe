da00ac8af9eb751aa3bd51ea92386b5102b193f2
SAKMADD <ahmadsafii.work@gmail.com>
2024-12-05 00:29:03 +0700

    save lagi bro

--- src/components/fragments/product-new/product-new-fields/tag-field-input.tsx
+++ src/components/fragments/product-new/product-new-fields/tag-field-input.tsx
@@ -1,38 +1,96 @@
 import { Field } from '@/components/ui/field';
 import { Tag } from '@/components/ui/tag';
-import { Flex } from '@chakra-ui/react';
+import { Flex, Input } from '@chakra-ui/react';
+import React, { useEffect, useRef, useState } from 'react';
 
 interface Props {
-  defaultValue?: string;
-  type?: string;
-  required?: boolean;
-  placeholder?: string;
-  label?: string;
+  label: string;
+  required: boolean;
 }
 
-export function TagFieldInput({ required, label }: Props) {
+export default function TagFieldInput({ label, required }: Props) {
+  const [varianOption, setVariantOption] = useState<string[]>([]);
+
+  useEffect(() => {
+    console.log(label, varianOption);
+  }, [label, varianOption]);
+
+  const [sizeInput, setSizeInput] = useState(1);
+  const ref_input = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    ref_input.current?.focus();
+
+    function handleKeyUp(event: KeyboardEvent) {
+      const newText = ref_input.current!.value.trim().replace(',', '');
+      switch (event.key) {
+        case ',':
+          if (newText.length > 0) {
+            setVariantOption((prev) => [...prev, newText]);
+            ref_input.current!.value = '';
+          } else {
+            ref_input.current!.value = '';
+          }
+          break;
+        case 'Enter':
+          if (newText.length > 0) {
+            setVariantOption((prev) => [...prev, newText]);
+            ref_input.current!.value = '';
+          }
+          break;
+        default:
+          break;
+      }
+    }
+
+    window.addEventListener('keyup', handleKeyUp);
+    return () => window.removeEventListener('keyup', handleKeyUp);
+  }, [sizeInput, varianOption]);
+
+  const handleChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    if (value.trim().length > 0) {
+      setSizeInput(value.length);
+    } else {
+      ref_input.current!.value = '';
+    }
+  };
+
+  function handleDelItem(index: number) {
+    setVariantOption((prev) => prev.filter((_, i) => i !== index));
+  }
+
   return (
-    <>
-      <Field label={label} required={required} color={'gray'}>
-        <Flex
-          gap={'0.5rem'}
-          cursor={'pointer'}
-          border={'1px solid #e6e6e6'}
-          width={'100%'}
-          padding={'0.5rem'}
-          borderRadius={'sm'}
-        >
-          <Tag closable size={'xl'}>
-            Closable Tag
+    <Field label={label} required={required} color={'gray'}>
+      <Flex
+        alignItems={'center'}
+        gap={'0.5rem'}
+        border={'1px solid #e6e6e6'}
+        width={'100%'}
+        padding={'0.5rem'}
+        borderRadius={'sm'}
+      >
+        {varianOption.map((text, i) => (
+          <Tag
+            key={text}
+            colorScheme="cyan"
+            closable
+            onClick={() => handleDelItem(i)}
+          >
+            {text}
           </Tag>
-          <Tag closable size={'xl'}>
-            Closable Tag
-          </Tag>
-          <Tag closable size={'xl'}>
-            Closable Tag
-          </Tag>
-        </Flex>
-      </Field>
-    </>
+        ))}
+        <Input
+          fontSize={'sm'}
+          height={'20%'}
+          position={'relative'}
+          border={'none'}
+          outline={'none'}
+          ref={ref_input}
+          onChange={handleChangeInput}
+          size={'lg'}
+        />
+      </Flex>
+    </Field>
   );
 }
--- src/components/fragments/product-new/product-new-sections/product-variant-list.tsx
+++ src/components/fragments/product-new/product-new-sections/product-variant-list.tsx
@@ -1,8 +1,13 @@
+import { VariantUIType } from '@/types/types';
 import { Box, Button, Flex, Spacer, Text } from '@chakra-ui/react';
 import { FaRegEdit } from 'react-icons/fa';
 import { ProductVariantItem } from './product-variant-item';
 
-export function ProductVariantListSection() {
+interface Props {
+  variantList: VariantUIType[];
+}
+
+export function ProductVariantListSection({ variantList }: Props) {
   return (
     <Flex flexDir="column" gap="1rem">
       <Flex alignItems={'center'}>
@@ -25,7 +30,10 @@ export function ProductVariantListSection() {
           Edit All Variant
         </Button>
       </Flex>
-      <ProductVariantItem />
+      {variantList.map(
+        (variant) =>
+          variant.is_checked && <ProductVariantItem key={variant.id} />
+      )}
     </Flex>
   );
 }
