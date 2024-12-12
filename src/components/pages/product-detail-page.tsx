5e8a75e86145558404432700c478e222c9d85e6a
SAKMADD <ahmadsafii.work@gmail.com>
2024-12-12 21:03:43 +0700

    save

--- src/components/pages/product-detail-page.tsx
+++ src/components/pages/product-detail-page.tsx
@@ -7,7 +7,7 @@ import { useState } from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { ProductDetailContent } from '../fragments/product-detail/product-content';
 import { ProductSpecification } from '../fragments/product-detail/productDetail/product-specification';
-import { Toaster } from '../ui/toaster';
+import { Toaster, toaster } from '@/components/ui/toaster';
 
 const product = dummyProductDetail;
 
@@ -22,6 +22,16 @@ export function ProductDetailPage() {
   });
 
   const onSubmit: SubmitHandler<ProductType> = (data) => {
+    if (selectedVariantOption.length < preparedProduct.variants!.length) {
+      toaster.create({
+        title: 'Pilih Semua Variasi Produk',
+        description: `Anda harus memilih semua ${preparedProduct.variants!.length} variasi produk sebelum melanjutkan.`,
+        duration: 3000,
+        type: 'error',
+      });
+      return;
+    }
+
     const checkoutProduct = {
       ...preparedProduct,
       selected_variant: selectedVariantOption,
