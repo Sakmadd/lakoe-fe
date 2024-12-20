3ce123ddebbb170e616e96509855eaa7fdbab055
SAKMADD <ahmadsafii.work@gmail.com>
2024-12-20 21:30:58 +0700

    save

--- src/hooks/use-product.ts
+++ src/hooks/use-product.ts
@@ -14,6 +14,7 @@ import Swal from 'sweetalert2';
 export function useProduct() {
   const navigate = useNavigate();
   const queryClient: QueryClient = useQueryClient();
+
   const { data: products, isLoading } = useQuery<SellerProductListType[]>({
     queryKey: ['products'],
     queryFn: api.GET_SHOP_PRODUCTS,
@@ -24,6 +25,26 @@ export function useProduct() {
     return ProductGrouper({ products });
   }, [products]);
 
+  async function CREATE_PRODUCT(data: FormData) {
+    return await api.CREATE_PRODUCT(data);
+  }
+
+  async function DELETE_PRODUCT(id: string[]) {
+    return await api.DELETE_PRODUCT_BATCH(id);
+  }
+
+  async function EDIT_PRICE_PRODUCT(id: string, amount: string[]) {
+    await api.EDIT_PRICE_SINGLE(id, amount);
+  }
+
+  async function EDIT_STOCK_PRODUCT(id: string, amount: string[]) {
+    await api.EDIT_STOCK_SINGLE(id, amount);
+  }
+
+  async function TOGGLE_PRODUCT(id: string[]) {
+    return await api.TOGGLE_ACTIVE_BATCH(id);
+  }
+
   const createProduct = useMutation({
     mutationFn: CREATE_PRODUCT,
     onSuccess: () => {
@@ -31,9 +52,35 @@ export function useProduct() {
     },
   });
 
-  async function CREATE_PRODUCT(data: FormData) {
-    return await api.CREATE_PRODUCT(data);
-  }
+  const deleteProduct = useMutation({
+    mutationFn: DELETE_PRODUCT,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['products'] });
+    },
+  });
+
+  const editPriceProduct = useMutation({
+    mutationFn: ({ id, amount }: { id: string; amount: string[] }) =>
+      EDIT_PRICE_PRODUCT(id, amount),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['products'] });
+    },
+  });
+
+  const editStockProduct = useMutation({
+    mutationFn: ({ id, amount }: { id: string; amount: string[] }) =>
+      EDIT_STOCK_PRODUCT(id, amount),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['products'] });
+    },
+  });
+
+  const toggleProduct = useMutation({
+    mutationFn: TOGGLE_PRODUCT,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['products'] });
+    },
+  });
 
   async function onPost(data: FormData): Promise<void> {
     Swal.fire({
@@ -56,17 +103,6 @@ export function useProduct() {
         });
       }
     });
-  }
-
-  const deleteProduct = useMutation({
-    mutationFn: DELETE_PRODUCT,
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['products'] });
-    },
-  });
-
-  async function DELETE_PRODUCT(id: string[]) {
-    return await api.DELETE_PRODUCT_BATCH(id);
   }
 
   async function onDelete(id: string[]): Promise<void> {
@@ -93,15 +129,50 @@ export function useProduct() {
     });
   }
 
-  const toggleProduct = useMutation({
-    mutationFn: TOGGLE_PRODUCT,
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['products'] });
-    },
-  });
+  async function onEditPrice(id: string, amount: string[]): Promise<void> {
+    Swal.fire({
+      title: 'Are you sure?',
+      html: 'Check before it’s too late!',
+      icon: 'question',
+      showCancelButton: true,
+      confirmButtonText: 'Yes, Edit!',
+      cancelButtonText: 'Cancel',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        editPriceProduct.mutate({ id, amount });
+        Swal.fire({
+          title: 'Price Edited!',
+          html: 'Let’s check the latest price!',
+          icon: 'success',
+          confirmButtonText: 'Go!',
+        }).then(() => {
+          navigate('/products');
+        });
+      }
+    });
+  }
 
-  async function TOGGLE_PRODUCT(id: string[]) {
-    return await api.TOGGLE_ACTIVE_BATCH(id);
+  async function onEditStock(id: string, amount: string[]): Promise<void> {
+    Swal.fire({
+      title: 'Are you sure?',
+      html: 'Check before it’s too late!',
+      icon: 'question',
+      showCancelButton: true,
+      confirmButtonText: 'Yes, Edit!',
+      cancelButtonText: 'Cancel',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        editStockProduct.mutate({ id, amount });
+        Swal.fire({
+          title: 'Price Edited!',
+          html: 'Let’s check the latest price!',
+          icon: 'success',
+          confirmButtonText: 'Go!',
+        }).then(() => {
+          navigate('/products');
+        });
+      }
+    });
   }
 
   async function onToggleProduct(id: string[]) {
@@ -132,6 +203,8 @@ export function useProduct() {
     products,
     onPost,
     onDelete,
+    onEditPrice,
+    onEditStock,
     onToggleProduct,
     groupedProducts,
     isLoading,
